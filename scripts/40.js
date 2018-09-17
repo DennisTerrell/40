0806b262df53607b4c4dc7dd64930dddf7479e6c
Dennis Terrell <dennisterrell@gmail.com>
2018-09-16 18:07:59 -0700

    Switched to sass, added counter

---
+++ scripts/40.js
@@ -0,0 +1,32 @@
+button = $('.card__button');
+
+button.on('click', function clicked() {
+	$(this).addClass('card__button--triggered');
+		
+	$(this).off('click', clicked);
+
+var count=40;
+var counter=setInterval(timer, 100);
+
+function timer()
+{
+  count -= 1;
+  if (count === -1)
+  {
+  	clearInterval(counter);
+		
+		setTimeout(function(){
+			count = 40;
+			document.getElementById("num").innerHTML=count;
+
+			button.removeClass('card__button--triggered');
+			button.on('click', clicked);
+			
+		}, 800);
+
+     return;
+  }
+ document.getElementById("num").innerHTML=count;
+}
+	
+});
