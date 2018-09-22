81e30c1d54740974bf5f283e3da81057172a885d
Dennis Terrell <dennisterrell@gmail.com>
2018-09-21 22:08:44 -0700

    formattting

--- scripts/40.js
+++ scripts/40.js
@@ -1,18 +1,15 @@
 $( document ).ready(function() {
-  
 
   button = $('.card__button');
 
   button.on('click', function clicked() {
 	  $(this).addClass('card__button--triggered');
-		
 	  $(this).off('click', clicked);
 
-  var count=1;
-  var counter=setInterval(timer, 100);
+    var count=1;
+    var counter=setInterval(timer, 100);
 
-  function timer()
-  {
+    function timer(){
     count += 1;
     if (count === 41)
     {
@@ -29,11 +26,14 @@ $( document ).ready(function() {
   
        return;
     }
-  document.getElementById("num").innerHTML=count;
-  window.onload = timer;
+    
+    document.getElementById("num").innerHTML=count;
+    window.onload = timer;
   }
  });
- $("#starter").addClass('card__button--triggered');
+ 
+  $("#starter").addClass('card__button--triggered');
+
 });
 
 
